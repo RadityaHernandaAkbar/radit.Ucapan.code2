ffe4f99473aa7c47d65cc76df77b87e9c070df40
RadityaHernandaAkbar <hernandaakbarraditya@gmail.com>
2024-09-21 18:24:33 +0800

    Add files via upload

---
+++ UcapanVar.java
@@ -0,0 +1,18 @@
+package tipedata;
+
+public class UcapanVar {
+
+    public static void main(String[] args) {
+
+        String universitas = "Halo ULM";
+        String unggul = "ULM Berdaya Saing dan Unggul di Bidang Lahan Basah";
+        String patir = "Prodi Ilkom ULM Unggul dengan Teknologi Kekinian";
+
+        System.out.println(universitas);
+        System.out.println(unggul);
+        System.out.println(patir);
+        System.out.println("----------------------------------------------------------");
+        System.out.println(universitas + "\r " + unggul);
+    }
+
+}
